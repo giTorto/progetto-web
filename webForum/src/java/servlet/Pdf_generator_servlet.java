4b4ef9a942909b0800aa3b6b97cf10da690e0dce
LoreBz <lolloghironf@hotmail.it>
2013-11-19 17:51:05 +0100

    pdf funzionante, ho cambiato la risposta della gruppiservlet che ora è semplicemente una pagina statica (ma figa) per ragioni di debug

---
+++ webForum/src/java/servlet/Pdf_generator_servlet.java
@@ -0,0 +1,97 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package servlet;
+
+import com.itextpdf.text.Document;
+import com.itextpdf.text.DocumentException;
+import com.itextpdf.text.Paragraph;
+import com.itextpdf.text.pdf.PdfWriter;
+import java.io.IOException;
+import java.util.Date;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class Pdf_generator_servlet extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+
+        response.setContentType("application/pdf");
+
+        Document document = new Document();
+
+        try {
+
+            PdfWriter.getInstance(document, response.getOutputStream());
+
+            document.open();
+
+            document.add(new Paragraph("prova pdf"));
+            document.add(new Paragraph(new Date().toString()));
+        } catch (DocumentException de) {
+            de.printStackTrace();
+            System.err.println("document: " + de.getMessage());
+        }
+
+        document.close();
+
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
--- webForum/src/java/servlet/gruppiSrvlt.java
+++ webForum/src/java/servlet/gruppiSrvlt.java
@@ -3,33 +3,25 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-
 package servlet;
+
 import db.DBManager;
-import db.Gruppo;
-import db.Utente;
 
 import java.io.IOException;
-import java.io.PrintWriter;
-import java.sql.SQLException;
-import java.util.List;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  *
  * @author Giulian
  */
 public class gruppiSrvlt extends HttpServlet {
-    
+
     private DBManager manager;
-    
-     @Override
+
+    @Override
     public void init() {
         // inizializza il DBManager dagli attributi di Application
         this.manager = (DBManager) super.getServletContext().getAttribute("dbmanager");
@@ -46,42 +38,43 @@ public class gruppiSrvlt extends HttpServlet {
      */
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
-        response.setContentType("text/html;charset=UTF-8");
-        PrintWriter out = response.getWriter();
-        
-        HttpSession session = request.getSession();
-        Utente user = (Utente) session.getAttribute("user");
-       
-      
-        
-       
-        try {
-            List<Gruppo> gruppiProp = manager.getGruppiOwner(user);
-            List<Gruppo> gruppiParte = manager.getGruppiPart(user);
-            
-            
-            /* TODO output your page here. You may use following sample code. */
-            out.println("<!DOCTYPE html>");
-            out.println("<html>");
-            out.println("<head>");
-            out.println("<title>Servlet gruppiSrvlt</title>");            
-            out.println("</head>");
-            out.println("<body>");
-            out.println("<h1>Lista gruppi</h1>");
-            int lung = gruppiParte.size();
-            Gruppo grup;
-            for(int i=0;i<lung;i++){
-                grup = gruppiParte.get(i);  
-                out.println("<tr> <td> "+ grup.getNome() +" </td> <td> "+grup.getOwnerName()+ "</td> <td>"+grup.getDataCreazione().toString()+ "</td> </tr>" );
-            }
-            out.println("");
-            out.println("</body>");
-            out.println("</html>");
-        } catch (SQLException ex) {
-            Logger.getLogger(gruppiSrvlt.class.getName()).log(Level.SEVERE, null, ex);
-        } finally {
-            out.close();
-        }
+        response.sendRedirect(request.getContextPath() + "/dist/gruppipage.html");
+//        response.setContentType("text/html;charset=UTF-8");
+//        PrintWriter out = response.getWriter();
+//        
+//        HttpSession session = request.getSession();
+//        Utente user = (Utente) session.getAttribute("user");
+//       
+//      
+//        
+//       
+//        try {
+//            List<Gruppo> gruppiProp = manager.getGruppiOwner(user);
+//            List<Gruppo> gruppiParte = manager.getGruppiPart(user);
+//            
+//            
+//            /* TODO output your page here. You may use following sample code. */
+//            out.println("<!DOCTYPE html>");
+//            out.println("<html>");
+//            out.println("<head>");
+//            out.println("<title>Servlet gruppiSrvlt</title>");            
+//            out.println("</head>");
+//            out.println("<body>");
+//            out.println("<h1>Lista gruppi</h1>");
+//            int lung = gruppiParte.size();
+//            Gruppo grup;
+//            for(int i=0;i<lung;i++){
+//                grup = gruppiParte.get(i);  
+//                out.println("<tr> <td> "+ grup.getNome() +" </td> <td> "+grup.getOwnerName()+ "</td> <td>"+grup.getDataCreazione().toString()+ "</td> </tr>" );
+//            }
+//            out.println("");
+//            out.println("</body>");
+//            out.println("</html>");
+//        } catch (SQLException ex) {
+//            Logger.getLogger(gruppiSrvlt.class.getName()).log(Level.SEVERE, null, ex);
+//        } finally {
+//            out.close();
+//        }
     }
 
     // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
