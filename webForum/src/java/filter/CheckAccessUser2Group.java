a7446346e14b1e65a7edfcd5fa9286d59b1b18e4
LoreBz <lolloghironf@hotmail.it>
2013-12-02 13:06:14 +0100

    filter su displaygroup in get funzionante: se non puoi accedere al gruppo specificato vieni reindirizzato su gruppisrvlt

---
+++ webForum/src/java/filter/CheckAccessUser2Group.java
@@ -0,0 +1,190 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package filter;
+
+import db.DBManager;
+import db.Gruppo;
+import db.Utente;
+import java.io.IOException;
+import java.io.PrintStream;
+import java.io.PrintWriter;
+import java.io.StringWriter;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.servlet.Filter;
+import javax.servlet.FilterChain;
+import javax.servlet.FilterConfig;
+import javax.servlet.ServletException;
+import javax.servlet.ServletRequest;
+import javax.servlet.ServletResponse;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+import servlet.gruppiSrvlt;
+
+/**
+ *
+ * @author Lorenzo
+ */
+public class CheckAccessUser2Group implements Filter {
+
+    private DBManager manager;
+    private static final boolean debug = true;
+    Utente user;
+    boolean userpuòaccedere = false;
+    // The filter configuration object we are associated with.  If
+    // this value is null, this filter instance is not currently
+    // configured. 
+    private FilterConfig filterConfig = null;
+
+    public CheckAccessUser2Group() {
+    }
+
+    /**
+     *
+     * @param request The servlet request we are processing
+     * @param response The servlet response we are creating
+     * @param chain The filter chain we are processing
+     *
+     * @exception IOException if an input/output error occurs
+     * @exception ServletException if a servlet error occurs
+     */
+    public void doFilter(ServletRequest request, ServletResponse response,
+            FilterChain chain)
+            throws IOException, ServletException {
+        userpuòaccedere = false;
+        if (debug) {
+            log("CheckAccessUser2Group:doFilter()");
+        }
+        String ingruppo = (request.getParameter("groupID"));
+
+        int idgruppo = Integer.parseInt(ingruppo);
+        HttpSession session = ((HttpServletRequest) request).getSession(false);
+        if (session != null) {
+            user = (Utente) session.getAttribute("user");
+        }
+
+        try {
+
+            ArrayList<Integer> ListidsPartecipanti = (ArrayList<Integer>) manager.getUtenti(idgruppo);
+            Gruppo gruppo = manager.getGruppo(idgruppo);
+            Utente owner = manager.getMoreByUserName(gruppo.getOwnerName());
+            ListidsPartecipanti.add(owner.getId());
+            if (ListidsPartecipanti.contains(user.getId())) {
+                userpuòaccedere = true;
+            }
+        } catch (SQLException ex) {
+            Logger.getLogger(gruppiSrvlt.class.getName()).log(Level.SEVERE, null, ex);
+            ((HttpServletResponse) response).sendRedirect("errorpage.html");
+        }
+
+        if (!userpuòaccedere) {
+            ((HttpServletResponse) response).sendRedirect(((HttpServletRequest) request).getContextPath() + "/logg/gruppiSrvlt ");
+        }
+        chain.doFilter(request, response);
+
+    }
+
+    /**
+     * Return the filter configuration object for this filter.
+     */
+    public FilterConfig getFilterConfig() {
+        return (this.filterConfig);
+    }
+
+    /**
+     * Set the filter configuration object for this filter.
+     *
+     * @param filterConfig The filter configuration object
+     */
+    public void setFilterConfig(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+    }
+
+    /**
+     * Destroy method for this filter
+     */
+    public void destroy() {
+    }
+
+    @Override
+    public void init(FilterConfig filterConfig) {
+        this.filterConfig = filterConfig;
+        if (filterConfig != null) {
+            if (debug) {
+                log("GroupChangeName:Initializing filter");
+            }
+        }
+
+        // inizializza il DBManager dagli attributi di Application
+        this.manager = (DBManager) filterConfig.getServletContext().getAttribute("dbmanager");
+    }
+
+    /**
+     * Return a String representation of this object.
+     */
+    @Override
+    public String toString() {
+        if (filterConfig == null) {
+            return ("CheckAccessUser2Group()");
+        }
+        StringBuffer sb = new StringBuffer("CheckAccessUser2Group(");
+        sb.append(filterConfig);
+        sb.append(")");
+        return (sb.toString());
+    }
+
+    private void sendProcessingError(Throwable t, ServletResponse response) {
+        String stackTrace = getStackTrace(t);
+
+        if (stackTrace != null && !stackTrace.equals("")) {
+            try {
+                response.setContentType("text/html");
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                PrintWriter pw = new PrintWriter(ps);
+                pw.print("<html>\n<head>\n<title>Error</title>\n</head>\n<body>\n"); //NOI18N
+
+                // PENDING! Localize this for next official release
+                pw.print("<h1>The resource did not process correctly</h1>\n<pre>\n");
+                pw.print(stackTrace);
+                pw.print("</pre></body>\n</html>"); //NOI18N
+                pw.close();
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        } else {
+            try {
+                PrintStream ps = new PrintStream(response.getOutputStream());
+                t.printStackTrace(ps);
+                ps.close();
+                response.getOutputStream().close();
+            } catch (Exception ex) {
+            }
+        }
+    }
+
+    public static String getStackTrace(Throwable t) {
+        String stackTrace = null;
+        try {
+            StringWriter sw = new StringWriter();
+            PrintWriter pw = new PrintWriter(sw);
+            t.printStackTrace(pw);
+            pw.close();
+            sw.close();
+            stackTrace = sw.getBuffer().toString();
+        } catch (Exception ex) {
+        }
+        return stackTrace;
+    }
+
+    public void log(String msg) {
+        filterConfig.getServletContext().log(msg);
+    }
+
+}
