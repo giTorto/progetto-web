a2aefa381c778da4fbcc1ee47706d4d3129d1ae5
giTorto <giulian.trt@gmail.com>
2013-12-02 16:13:49 +0100

    ora la download file... scarica il file :P (se 2 utenti mettono file uguale prende quello con data maggiore!)

--- webForum/src/java/servlet/FileDownload.java
+++ webForum/src/java/servlet/FileDownload.java
@@ -9,6 +9,7 @@ package servlet;
 import db.DBManager;
 import java.io.File;
 import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.PrintWriter;
@@ -22,7 +23,7 @@ import javax.servlet.ServletOutputStream;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import static servlet.FileDownload1.showError;
+
 
 /**
  *
@@ -86,7 +87,7 @@ DBManager manager=null;
     try {
         dbData = manager.getRealAndDBName(Integer.parseInt(fileId)); //qui va fatto un filtro sui idpost,idgruppo e idutente
     } catch (SQLException ex) {
-        Logger.getLogger(FileDownload1.class.getName()).log(Level.SEVERE, null, ex);
+        Logger.getLogger(FileDownload.class.getName()).log(Level.SEVERE, null, ex);
     }
         String path=dbData.get("path");
         String name=dbData.get("name");
@@ -99,10 +100,7 @@ DBManager manager=null;
         String mimeType = ctx.getMimeType(download.getAbsolutePath());
 	response.setContentType(mimeType != null? mimeType:"application/octet-stream");
 	response.setContentLength((int) download.length());
-        response.setHeader("Content-Disposition", "inline; filename=\"" + name + "\"");
-        //scambiare riga sopra con la seconda riga sotto per download 
-        //diretto invece di provarlo ad aprire nel browser
-        //response.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
+        response.setHeader("Content-Disposition", "attachment; filename=\"" + name + "\"");
         ServletOutputStream os= response.getOutputStream();
         byte[] bufferData = new byte[1024];
         int read=0;
@@ -137,5 +135,12 @@ DBManager manager=null;
     public String getServletInfo() {
         return "Short description";
     }// </editor-fold>
+    
+      static public void showError() throws FileNotFoundException{
+        //scegli cosa fare, o reindirizzi o mandi l'errore (oppure reindirizzi ad una pagina con un messaggio d'errore, che crei tu, tipo 404)
+        throw new FileNotFoundException("You do not have priviliegies or the file is missing");
+        //response.sendRedirect(request.getContextPath() + "/index.html"); 
+
+    }
 
 }
